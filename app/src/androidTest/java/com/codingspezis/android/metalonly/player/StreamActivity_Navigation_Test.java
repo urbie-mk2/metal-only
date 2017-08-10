e739bbc48afa24e203960424cbe7068387fe0a38
Jan Lippert <lippertsjan@gmail.com>
2017-08-11 00:34:33 +0200

    Removed unused imports in StreamActivity_Navigation_Test.java

--- app/src/androidTest/java/com/codingspezis/android/metalonly/player/StreamActivity_Navigation_Test.java
+++ app/src/androidTest/java/com/codingspezis/android/metalonly/player/StreamActivity_Navigation_Test.java
@@ -7,8 +7,6 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.codingspezis.android.metalonly.player.stream.StreamPlayerInternal;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +16,6 @@ import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
