6daa62225dddec49988641e815e3a73bb896934f
Jan Lippert <lippertsjan@gmail.com>
2017-08-11 00:01:30 +0200

    Added test for navigation from StreamActivity to About

---
+++ app/src/androidTest/java/com/codingspezis/android/metalonly/player/StreamActivity_Navigation_Test.java
@@ -0,0 +1,48 @@
+package com.codingspezis.android.metalonly.player;
+
+import android.content.ComponentName;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.codingspezis.android.metalonly.player.stream.StreamPlayerInternal;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class StreamActivity_Navigation_Test {
+    @Rule
+    public ActivityTestRule<StreamControlActivity_> mActivityTestRule = new ActivityTestRule<>(StreamControlActivity_.class);
+
+    @Test
+    public void aboutActivity_Navigation_Test() {
+        Intents.init();
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.title), withText("Info"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        intended(hasComponent(new ComponentName(getTargetContext(), AboutActivity_.class)));
+    }
+
+}
