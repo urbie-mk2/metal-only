f9df0733c5506bc6c544c7eb30f37c5c25c5d3c2
Jan Lippert <lippertsjan@gmail.com>
2017-08-11 10:29:24 +0200

    Removed android sdk update

--- scripts/travis_start_emulator.sh
+++ scripts/travis_start_emulator.sh
@@ -1,4 +1,3 @@
-android-update-sdk --components=sys-img-$ABI-$API --accept-licenses='android-sdk-license-[0-9a-f]{8}'
 echo no | android create avd --force -n test -t $API --abi $ABI
 emulator -avd test -no-audio -no-window
 android-wait-for-emulator
