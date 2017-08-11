ad8c76ee90de279fdbf38b3f69bbe0128947841f
Jan Lippert <lippertsjan@gmail.com>
2017-08-11 11:57:31 +0200

    Creating correct emulator now

--- scripts/travis_start_emulator.sh
+++ scripts/travis_start_emulator.sh
@@ -1,4 +1,4 @@
-echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
