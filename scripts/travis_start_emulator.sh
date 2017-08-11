5913380fe79aee5ac460758f5a712e98a8dfed53
Jan Lippert <lippertsjan@gmail.com>
2017-08-11 10:48:40 +0200

    Next try on integration via travis

--- scripts/travis_start_emulator.sh
+++ scripts/travis_start_emulator.sh
@@ -1,4 +1,4 @@
-echo no | android create avd --force -n test -t $API --abi $ABI
-emulator -avd test -no-audio -no-window
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
